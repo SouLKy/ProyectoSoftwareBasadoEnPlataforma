d76f9fd2a121dd324ee4ce2027951c7f895013e7
seba6453 <sebastian.astudillo@alumnos.ucn.cl>
2022-06-20 17:56:49 -0400

    page contact
    pagina terminada, solo falta ingresar informacion real que se desplegara en la pagina

--- client/src/pages/Contact/index.jsx
+++ client/src/pages/Contact/index.jsx
@@ -1,10 +1,31 @@
 import React from "react";
-import { Tutorial } from "./styles";
+import { Title,Post,Text1,Text2,Subtitle } from "./styles";
+
 
 const Contact = () =>{
+
+    const listContact = ['soporte@gmail.com','+569 87654321','+51 2 234533']
+
     return (
         <>
-            <Tutorial>Hola</Tutorial>
+            <Title>Contacto</Title>
+            <Text1>Cualquier duda contactarse a los siguientes canales:</Text1>
+            <Post>
+                <Subtitle>Correo electronico</Subtitle>
+                <Text2>{listContact[0]}</Text2>
+            </Post>
+            <Post>
+                <Subtitle>Numero telefonico</Subtitle>
+                <Text2>{listContact[1]}</Text2>
+                <Text2>{listContact[2]}</Text2>
+            </Post>
+            <Post>
+                <Subtitle>Redes Sociales</Subtitle>
+                <Text2>Instagram</Text2>
+                <Text2>Facebook</Text2>
+                <Text2>Twitter</Text2>
+            </Post>
+            
         </>
     )
 }
--- client/src/pages/Contact/styles.jsx
+++ client/src/pages/Contact/styles.jsx
@@ -1,5 +1,35 @@
 import styled from "styled-components";
 
-export const Tutorial = styled.h1`
-    color:red;
+
+
+export const Title = styled.h1`
+    color:#224957;
+    padding:10px;
+    margin:auto;
+`;
+
+export const Subtitle = styled.h3`
+    padding:5px;
+`;
+
+export const Text1 = styled.p`
+    padding:10px;
+    margin:auto;
+`;
+
+export const Text2 = styled.p`
+    padding:10px;
+    margin:auto;
+    font-style:italic;
+`;
+
+export const Post = styled.div`
+    padding: 10px 20px;
+    margin: 0 0 20px;
+    border-left: 5px solid #b2b2b2;
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 40px;
+    margin-inline-end: 40px;
 `;
