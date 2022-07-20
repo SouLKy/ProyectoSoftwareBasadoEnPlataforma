e73f501e6488c232360987ca7d7e2a0bd233c93f
Angelo <angelo.berriosgrafit@gmail.com>
2022-07-19 23:02:27 -0400

    SolucionandoError

--- client/src/pages/Test/index.jsx
+++ client/src/pages/Test/index.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import AccountBank from "../../services/AccountBank";
 
 const Test = () =>{
+
     const ObtenerCookie = ()=>{
         const cookies = document.cookie.split(';').map(cookie =>
             cookie.split('=')).reduce((accumulator, [key, value]) =>
@@ -10,8 +11,10 @@ const Test = () =>{
             )
         return cookies.token
     }
-    console.log(ObtenerCookie)
-    AccountBank({ObtenerCookie}).then(res =>{
+
+    const [cookie, setCookie] = useState(()=>ObtenerCookie());
+
+    AccountBank(cookie).then(res =>{
         console.log(res.data)
     }).catch( err =>{
         alert("error")
