4bdc7ee7677e3203da2f7d029765a211c3207861
seba6453 <sebastian.astudillo@alumnos.ucn.cl>
2022-07-20 13:35:48 -0400

    Update home
    pagina de home responsiva

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,59 +1,50 @@
-import React, {useState, useEffect} from 'react';
-import {Title, SubTitle, Box, Imagen, ButtonReg, Container} from './styles';
+import React from 'react';
+import {Title, SubTitle, Imagen, ButtonReg, Container,Container2, Title2, Post, Text2, Subtitle, ContainerCol2} from './styles';
 
 
 const Home = () => {
-
-
-  /*
-  const images = ["https://picsum.photos/800/800/?blur","https://cms-assets.tutsplus.com/cdn-cgi/image/width=360/uploads/users/1125/posts/30546/preview_image/RN.jpg"]
-
-  
-
-  const [Countt, setCount] = useState(0);
-  const [Image, setImage] = useState([images[0]]);
-
-  useEffect(() => {
-    if (Countt>2){
-      setCount(0);
-      setImage(images[Countt])
-    }
-    else if (Countt<0){
-      setCount(2);
-      setImage(images[Countt])
-    }
-    else {
-      setImage(images[Countt])
-    }
-  },);
-  */
+  const listContact = ['soporte@gmail.com','+569 87654321','+51 2 234533']
 
   return (
-    /*
-    <>
-    <ContentImages>
-      <Arrow onClick={() =>
-        setCount(Countt-1)} Right="30px solid rgb(34,73,87)">
-      </Arrow>
-      <BackgroundImage url={Image}>
-      </BackgroundImage>
-      <Arrow onClick={() =>
-        setCount(Countt+1)} Left="30px solid rgb(34,73,87)">
-      </Arrow>
-    </ContentImages>
 
-    <Go href = "login" type ='button' background='rgba(34, 73, 87, 100%)' color="#fff" value="Iniciar Sesión" display="block">Iniciar Sesión</Go>
+    <>
+      <Container>
+        <Container>
+          <Imagen></Imagen>
+        </Container>
+        <ContainerCol2>
+          <Title>Controla las finanzas de tu empresa</Title>
+          <SubTitle>Carga tus cartolas al sistema y revisa las transacciones de tu empresa, todo en la palma de tu mano.</SubTitle>
+          <ButtonReg>REGISTRATE AHORA</ButtonReg>
+        </ContainerCol2>
+      </Container>
+
+      <Title2>Canales de contacto</Title2>
+
+      <Container2>
+        <Container2>
+          <Post>
+              <Subtitle>Correo electronico</Subtitle>
+              <Text2>{listContact[0]}</Text2>
+          </Post>
+          <Post>
+              <Subtitle>Numero telefonico</Subtitle>
+              <Text2>{listContact[1]}</Text2>
+              <Text2>{listContact[2]}</Text2>
+          </Post>
+          <Post>
+              <Subtitle>Redes Sociales</Subtitle>
+              <Text2>Instagram</Text2>
+              <Text2>Facebook</Text2>
+              <Text2>Twitter</Text2>
+          </Post>
+        </Container2>
+      </Container2>
+      <br></br>
+      <br></br>
+      <br></br>
     </>
-    */
-   <>
-    <Container>
-        <Title>Controla las finanzas de tu empresa</Title>
-        <SubTitle>Te ayudamos a realizar tu conciliacion bancaria</SubTitle>
-        <ButtonReg>REGISTRATE AHORA</ButtonReg>
-        <Imagen></Imagen>
-    </Container>
-   </>
-  );
+    );
 };
 
 export default Home;
--- client/src/pages/Home/styles.jsx
+++ client/src/pages/Home/styles.jsx
@@ -1,66 +1,6 @@
 import styled from "styled-components";
 import { IndexPage } from "../../components/icons/iconIndexPage";
 
-export const ContentImages = styled.div`
-  padding-top: 20px;
-  display:flex; 
-  margin:auto;
-
-  @media(min-width: 700px){
-    width:100%;
-    margin-top:50px;
-  }
-  @media (max-width:700px) {
-    width:100%; 
-    
-    padding-bottom: 20px;
-  }
-`;
-
-export const Arrow = styled.button`
-  border:0;
-  background:none;
-  width: 0;
-  height: 0;
-  margin: auto;
-  border-top: 18px solid transparent;
-  border-bottom: 18px solid transparent;
-  border-left : ${props=>props.Left};
-  border-right : ${props=>props.Right};
-  display:inline-block;
-  cursor: pointer;
-`;
-
-export const BackgroundImage = styled.div`
-  margin:auto;
-
-  
-`
-export const Go = styled.a`
-  display:${props => props.display};
-  padding : 3px;
-  margin:auto;
-  margin-top:20px;
-  max-width:600px;
-  min-width:300px;
-  width : 60%;
-  height: 40px;
-
-  text-decoration:none;
-  border:none;
-
-  border-radius: 10px;
-  font-size:24px;
-  text-align: center;
-  background: ${props => props.background};
-  :hover {
-    background : #0004;
-  }
-
-
-  color : ${props => props.color};
-`;
-
 export const Title = styled.h1`
   padding: 20px;
 
@@ -73,8 +13,6 @@ export const Title = styled.h1`
   text-transform: uppercase;
   
   color: #000;
-
-  grid-area: Title;
 `;
 
 export const SubTitle = styled.h4`
@@ -82,21 +20,18 @@ export const SubTitle = styled.h4`
   font-family: 'Arial';
   font-style: normal;
   font-weight: 55;
-
-  grid-area: SubTitle;
 `;
 
 export const Imagen = styled(IndexPage)`
-  width: 614px;
-  height: 487px;
   @media screen and (max-width: 768px){
-    width: 300px;
+    width: 100%;
+    height: 100%;
   }
 
   @media screen and (min-width: 769px){
-    width: 614px;
+    width: 100%;
+    height: 460px;
   }
-  grid-area: Imagen;
 `;
 
 export const Button = styled.button`
@@ -123,18 +58,69 @@ export const ButtonReg = styled(Button)`
     color: black;
   };
   border-radius: 30px;
-  grid-area: ButtonReg;
+  @media screen and (max-width: 768px){
+    width: 300px;
+  }
+
+  @media screen and (min-width: 769px){
+    width: 400px;
+  }
 `;
 
 export const Container = styled.div`
-  display: grid;
+  display: flex;
   height: 100%;
-  background-color: #47B5FF; 
-  grid-template-columns: 1.2fr 1fr;
-  grid-template-rows: 0.1fr 0.1fr 0.1fr 0.5fr;
-  grid-template-areas:
-            "nav Imagen"
-            "Title Imagen"
-            "SubTitle Imagen"
-            "ButtonReg Imagen";
+  background-color: #47B5FF;
+  flex-wrap: wrap;
+`;
+
+export const Container2 = styled(Container)`
+  background-color: #DFF6FF;
+  flex-direction: row;
+  justify-content: center;
+`;
+export const ContainerCol2 = styled(Container)`
+  flex-direction: column;
+  @media screen and (max-width: 768px){
+    justify-content: center;
+  }
+  
+`
+
+export const Title2 = styled.h1`
+    color:#224957;
+    padding:10px;
+    margin:auto;
+    text-align: center;
+`;
+
+export const Subtitle = styled.h3`
+    padding:5px;
+`;
+
+export const Text2 = styled.p`
+    padding:10px;
+    margin:auto;
+    font-style:italic;
+`;
+
+export const Post = styled.div`
+    padding: 10px;
+    margin: 0 0 20px;
+    border-left: 5px solid #06283D;
+    background-color: #47B5FF;
+    border-radius: 15px;
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 30px;
+    margin-inline-end: 30px;
+    flex-direction: column;
+    @media screen and (max-width: 768px){
+      width: 250px;
+    }
+  
+    @media screen and (min-width: 769px){
+      width: 400px;
+    }
 `;
