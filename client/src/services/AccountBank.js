e63a7f3def088c33ea223e1a02d51372e9825b41
Angelo <angelo.berriosgrafit@gmail.com>
2022-07-19 22:08:05 -0400

    Services AccountBank

---
+++ client/src/services/AccountBank.js
@@ -0,0 +1,16 @@
+const ENDPOINT = 'http://localhost:3001'
+
+export default function AccountBank ({}) {
+    return fetch(`${ENDPOINT}/accountBank`,{
+        method: 'POST',
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({})
+    }).then(res => {
+        if(!res.ok) throw new Error('error response')
+        return res.json()
+    }).then(res=>{
+        return {bancos, id} = res
+    })
+}
