9a876959103aa688f068385949e042ff3b2e0881
Angelo Berrios <angelo.berriosgrafit@gmail.com>
2022-08-01 04:31:37 -0400

    Cambios estilo en home

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useRef, useEffect} from 'react';
 import {Title, SubTitle, Imagen, ButtonReg, Container,Container2, Title2, Post, Text2, Subtitle, ContainerCol2} from './styles';
 import { useNavigate} from "react-router-dom";
 
+
+
 const Home = () => {
   let navigate = useNavigate();
-  
+  const contentRef = useRef();
+
   const Go = () =>{
     navigate("../Register", {replace: true})
   }
+
   const listContact = ['soporte@gmail.com','+569 87654321','+51 2 234533']
 
+  const [animation, setAnimation] = useState(false)
 
+  const handleScroll = () =>{
+    const div = contentRef.current
+    const distance = (div.getBoundingClientRect())
+    setAnimation(distance.top < (window.innerHeight || document.documentElement.clientHeight) && distance.bottom > 0)
+  }
+
+  useEffect(() => {
+    handleScroll()
+    window.addEventListener('scroll', handleScroll)
+
+    return () =>{
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, []);
+  //className={`texts ${hover?'active':''}`}
   return (
 
     <>
@@ -23,29 +43,22 @@ const Home = () => {
         </ContainerCol2>
       </Container>
 
-      <Title2>Canales de contacto</Title2>
+      <Title2 >Canales de contacto</Title2>
 
       <Container2>
-        <Container2>
-          <Post>
-              <Subtitle>Correo electronico</Subtitle>
-              <Text2>{listContact[0]}</Text2>
+          <Post ref={contentRef} className={`animation ${animation}`} >
+            <Subtitle >Numero telefonico</Subtitle>
+            <Text2 >{listContact[1]}</Text2>
+            <Text2>{listContact[2]}</Text2>
           </Post>
-          <Post>
-              <Subtitle>Numero telefonico</Subtitle>
-              <Text2>{listContact[1]}</Text2>
-              <Text2>{listContact[2]}</Text2>
+
+          <Post className={`animation2 ${animation}`}>
+            <Subtitle >Redes Sociales</Subtitle>
+            <Text2 >Instagram @...</Text2>
+            <Text2 >Facebook ...</Text2>
+            <Text2 >Twitter ...</Text2>
           </Post>
-          <Post>
-              <Subtitle>Redes Sociales</Subtitle>
-              <Text2>Instagram</Text2>
-              <Text2>Facebook</Text2>
-              <Text2>Twitter</Text2>
-          </Post>
-        </Container2>
       </Container2>
-      <br></br>
-      <br></br>
       <br></br>
     </>
     );
--- client/src/pages/Home/styles.jsx
+++ client/src/pages/Home/styles.jsx
@@ -78,11 +78,11 @@ export const Container2 = styled(Container)`
   background-color: #DFF6FF;
   flex-direction: row;
   justify-content: center;
+  padding-bottom:100px;
 `;
 export const ContainerCol2 = styled(Container)`
   flex-direction: column;
   justify-content: center;
-  
   @media screen and (max-width: 1320px){
     flex:1;
   }
@@ -94,23 +94,36 @@ export const Title2 = styled.h1`
     padding:10px;
     margin:auto;
     text-align: center;
+    margin-top:50px;
+    margin-bottom:50px;
 `;
 
 export const Subtitle = styled.h3`
-    padding:5px;
+
+    margin:auto;
+    padding:10px;
+    opacity:1;
+    transition: 0.6s;
+    text-decoration: underline solid #0a0a0a2b;
 `;
 
 export const Text2 = styled.p`
     padding:10px;
     margin:auto;
+    
     font-style:italic;
+    &.active{
+      opacity:1;
+    }
+    transition: 0.6s;
 `;
+
 
 export const Post = styled.div`
     padding: 10px;
     margin: 0 0 20px;
     border-left: 5px solid #06283D;
-    background-color: #47B5FF;
+    background-color: transparent;
     border-radius: 15px;
     display: block;
     margin-block-start: 1em;
@@ -118,11 +131,38 @@ export const Post = styled.div`
     margin-inline-start: 30px;
     margin-inline-end: 30px;
     flex-direction: column;
+
+    opacity:0;
+
+
     @media screen and (max-width: 768px){
+      transform: translate(-1000px, 0px);
       width: 250px;
+      &.animation2{
+        transition:transform 1.5s cubic-bezier(.02,.97,.39,.79) 0.7s, background-color 1.5s ease 1s;
+      }
+      &.animation{
+        transition:transform 1.5s cubic-bezier(.02,.97,.39,.79), background-color 1.5s ease;
+      }
     }
   
     @media screen and (min-width: 769px){
+      transform: translate(-3000px, 0px);
       width: 400px;
+      transition:transform 1s cubic-bezier(.02,.97,.39,.79), background-color 1.5s ease;
     }
+
+    &.animation.true{
+      opacity:1;
+      transform: translate(0px, 0px);
+      background-color: #47B5FF;
+    }
+
+    &.animation2.true{
+      opacity:1;
+      transform: translate(0px, 0px);
+      background-color: #47B5FF;
+    }
+
+
 `;
